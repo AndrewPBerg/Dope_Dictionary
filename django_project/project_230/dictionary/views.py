2a92d1aaa17afbcbdaf4ec3ef637015a99afba76
AndrewPBerg <andberg9@gmail.com>
2024-11-18 18:52:10 -0500

    removed CSRF token (not important for local use)

--- django_project/project_230/dictionary/views.py
+++ django_project/project_230/dictionary/views.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 from pathlib import Path
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     return render(request, "home.html")
@@ -50,6 +51,7 @@ def get_definition_llm(style, word):
     except Exception as e:
         return f"Error generating definition: {str(e)}"
 
+@csrf_exempt
 def get_definition_service(request): 
     if request.method == "GET":
         try:
